36924cd13b0fde887fdcf74d5ed789d00f383164
noaheclifford <noaheclifford@gmail.com>
2026-09-15 14:50:59 -0500

    Update Resource.cpp broken
    broken but progress

---
+++ src/Resource.cpp
@@ -0,0 +1,51 @@
+#include "Resource.h"
+
+public resource() {
+	resourceID = -1;
+	resourceName = "none";
+	resourceType = "none";
+	availability = false;
+}
+
+public resource(int resourceID, string resourceName, string resourceType, bool availability) {
+	this.resourceID = resourceID;
+	this.resourceName = resourceName;
+	this.resourceType = resourceType;
+	this.availability = availability;
+}
+
+
+int setResourceID() {
+	return this.resourceID;
+}
+
+void getResourceID(int resourceID) {
+	this.resourceID = resourceID;
+}
+
+
+string setResourceName() {
+	return this.resourceName;
+}
+
+void getResourceName(string resourceName) {
+	this.resourceName = resourceName;
+}
+
+
+string setResourceType() {
+	return this.resourceType;
+}
+
+void getResourceType(string resourceType) {
+	this.resourceType = resourceType;
+}
+
+
+bool setAvailability() {
+	return this.availability;
+}
+
+void getAvailability(bool availability) {
+	this.availability = availability;
+}
