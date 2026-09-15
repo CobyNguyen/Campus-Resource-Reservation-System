1a6e77ea5f592f9b87593f3afff7c63200da4e59
noaheclifford <noaheclifford@gmail.com>
2026-09-15 14:13:09 -0500

    Update Resource.h description

---
+++ include/Resource.h
@@ -0,0 +1,13 @@
+//Resources may include:
+
+//Study Rooms
+//Laptops
+//Calculators
+//Lab Equipment
+//Tutoring appointments
+//Each resource must contain:
+
+//Resource ID
+//Resource Name
+//Resource Type
+//Availability Status
