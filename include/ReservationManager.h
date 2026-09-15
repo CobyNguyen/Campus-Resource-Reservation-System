97d6c9580158a77dd71d85bab1a80fccfe0be7a0
noaheclifford <noaheclifford@gmail.com>
2026-09-15 14:10:54 -0500

    Update ReservationManager.h description

---
+++ include/ReservationManager.h
@@ -0,0 +1,32 @@
+//Load resource data from input files.
+//Store information about available resources.
+//Display all resources.
+//Search for resources.
+//Sort resources according to user-selected criteria.
+
+//Resources may include:
+//Study Rooms
+//Laptops
+//Calculators
+//Lab Equipment
+//Tutoring appointments
+
+//Each resource must contain:
+//Resource ID
+//Resource Name
+//Resource Type
+//Availability Status
+//Reservation Management
+
+//Users shall be able to:
+//Create a reservation.
+//Cancel a reservation.
+//View current reservations.
+//Search for reservations.
+
+//Each reservation must contain:
+//Reservation ID
+//Student ID
+//Student Name
+//Resource ID
+//Reservation Date
