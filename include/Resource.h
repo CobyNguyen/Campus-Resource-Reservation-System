69a0a8a7cf7e3935513702c9f1fea370db77e05e
noaheclifford <noaheclifford@gmail.com>
2026-09-15 14:39:37 -0500

    Update Resource.h

--- include/Resource.h
+++ include/Resource.h
@@ -11,3 +11,35 @@
 //Resource Name
 //Resource Type
 //Availability Status
+
+#ifdef RESOURCE_H
+#define RESOURCE_H
+
+#include <string>
+
+public class resource {
+	private{
+		int resourceID;
+		string resourceName;
+		string resrouceType;
+		bool availability;
+	}
+	public{
+		public resource();
+		public resource(int, string, string, bool);
+
+		int setResourceID();
+		void getResourceID(int);
+
+		string setResourceName();
+		void getResourceName(string);
+
+		string setResrouceType();
+		void getResrouceType(string);
+
+		bool setAvailability();
+		void getAvailability(bool);
+	}
+}
+
+#endif
