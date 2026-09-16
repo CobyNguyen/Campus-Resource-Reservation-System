33384dd1b03feba770402ce5521293dd6e274b31
AayusTamang <aayustamang21@gmail.com>
2026-09-16 14:15:56 +0545

    Define cancellation history stack interface

---
+++ include/CancellationHistory.h
@@ -0,0 +1,27 @@
+#ifndef CANCELLATION_HISTORY_H
+#define CANCELLATION_HISTORY_H
+
+#include <ostream>
+#include <stack>
+#include <string>
+
+struct CancelledReservation {
+    int reservationId;
+    int studentId;
+    std::string studentName;
+    std::string resourceId;
+    std::string reservationDate;
+};
+
+class CancellationHistory {
+public:
+    void record(const CancelledReservation& reservation);
+    bool undo(CancelledReservation& reservation);
+    void display(std::ostream& out) const;
+    bool empty() const;
+
+private:
+    std::stack<CancelledReservation> cancelled_;
+};
+
+#endif
