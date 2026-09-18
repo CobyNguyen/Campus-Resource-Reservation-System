0e4c802e63489905c08dbbc9ddf986ca183160cb
AayusTamang <aayustamang21@gmail.com>
2026-09-18 07:13:23 +0545

    Track resource and time in waiting-list entries

--- include/WaitingList.h
+++ include/WaitingList.h
@@ -8,16 +8,22 @@
 struct WaitingStudent {
     int studentId;
     std::string studentName;
+    std::string resourceId;
+    std::string reservationDate;
+    std::string startTime;
+    std::string endTime;
 };
 
 class WaitingList {
 public:
+    explicit WaitingList(const std::string& resourceId);
     bool add(const WaitingStudent& student);
     bool removeNext(WaitingStudent& student);
     void display(std::ostream& out) const;
     bool empty() const;
 
 private:
+    std::string resourceId_;
     std::queue<WaitingStudent> students_;
 };
 
